dd945cde613d1b9e25a85fe5030d343b29fda99d
Manoj Phuyal <m.phuyal@wpguitgevers.nl>
2024-11-19 22:57:12 +0545

    added controller and connect to business logics

---
+++ JobCandidateHub.API/Controllers/CandidateController.cs
@@ -0,0 +1,31 @@
+﻿using JobCandidateHub.Database.Models;
+using JobCandidateHub.Service.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidateHub.API.Controllers;
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class CandidateController : ControllerBase
+{
+    private readonly ICandidateBusiness _candidateBusiness;
+
+    public CandidateController(ICandidateBusiness candidateBusiness)
+    {
+        _candidateBusiness = candidateBusiness;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Save(Candidate candidate)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var isSuccess = await _candidateBusiness.CreateOrUpdate(candidate);
+        if (isSuccess)
+        {
+            return Ok(candidate);
+        }
+        return BadRequest();
+    }
+}
+
